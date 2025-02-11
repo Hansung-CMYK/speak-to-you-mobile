bfa9e9b6ec6fc29cd09f1678d85f90a2421e3582
6-keem <6ukeem@gmail.com>
2025-02-12 01:48:12 +0900

    refactor: migrate state management from Provider to Riverpod

--- lib/providers/ex_user_provider.dart
+++ lib/providers/ex_user_provider.dart
@@ -1,22 +1,14 @@
-import 'package:flutter/foundation.dart';
-import '../models/ex_user_model.dart';
-
-class UserProvider with ChangeNotifier {
-  UserModel? _user; // 사용자 정보 저장
-  bool _isLoading = false;
-
-  UserModel? get user => _user;
-  bool get isLoading => _isLoading;
-
-  // 사용자 데이터 업데이트
-  void setUser(UserModel user) {
-    _user = user;
-    notifyListeners();
-  }
-
-  // 로딩 상태 변경
-  void setLoading(bool loading) {
-    _isLoading = loading;
-    notifyListeners();
-  }
-}
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import '../models/user_model.dart';
+//
+// final userProvider = StateNotifierProvider<UserNotifier, UserModel?>(
+//       (ref) => UserNotifier(),
+// );
+//
+// class UserNotifier extends StateNotifier<UserModel?> {
+//   UserNotifier() : super(null);
+//
+//   void setUser(UserModel user) {
+//     state = user;
+//   }
+// }
