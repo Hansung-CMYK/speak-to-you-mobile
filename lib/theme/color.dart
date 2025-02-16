b2ca0deda800abb2bff5bbdba484718e2b141c1d
6-keem <6ukeem@gmail.com>
2025-02-17 00:43:31 +0900

    chore: Create color.dart and theme.dart

---
+++ lib/theme/color.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+/// Figma에 정의되지 않은 색상은 다음을 참고
+///
+/// https://v3.tailwindcss.com/docs/customizing-colors
+///
+/// ```
+/// 1. 중복 선언 금지
+/// 2. 연한 색에서 진한 색으로 선언
+/// ```
+///
+class AppColors {
+  static const Color primary = Color(0xFFFE8745);
+  static const Color accent = Color(0xFF5865F2);
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color gray100 = Color(0xFFF7F8F9);
+  static const Color gray200 = Color(0xFFE8EBED);
+  static const Color gray300 = Color(0xFFC9CDD2);
+  static const Color gray400 = Color(0xFF9EA4AA);
+  static const Color gray600 = Color(0xFF72787F);
+  static const Color gray700 = Color(0xFF454C53);
+  static const Color gray800 = Color(0xFF26282B);
+  static const Color gray900 = Color(0xFF1E1E1E);
+
+  static const Color successLight = Color(0xFF4ADE80);
+  static const Color successBase = Color(0xFF22C55E);
+  static const Color successDark = Color(0xFF16A34A);
+
+  static const Color warningLight = Color(0xFFFDE047);
+  static const Color warningBase = Color(0xFFFACC15);
+  static const Color warningDark = Color(0xFFEAB308);
+
+
+  static const Color errorLight = Color(0xFFFF7171);
+  static const Color errorBase = Color(0xFFFF4747);
+  static const Color errorDark = Color(0xFFDD3333);
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,83 @@
+import 'package:ego/theme/color.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: AppColors.primary,
+
+      colorScheme: ColorScheme(
+        brightness: Brightness.light,
+        primary: AppColors.primary,
+        onPrimary: AppColors.white, // onPrimary 는 primary 배경 위 텍스트 색상
+
+        secondary: AppColors.accent,
+        onSecondary: AppColors.white,
+
+        surface: AppColors.white, // 배경색
+        onSurface: AppColors.gray900, // 전경색
+        onSurfaceVariant: AppColors.gray300, // 비활성 버튼 배경색
+
+        error: AppColors.errorBase,
+        onError: AppColors.white,
+      ),
+
+      scaffoldBackgroundColor: AppColors.white,
+
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.white,
+        foregroundColor: AppColors.gray900,
+        elevation: 0,
+      ),
+
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+          color: AppColors.gray900,
+          fontSize: 16,
+        ),
+      ),
+
+      iconTheme: const IconThemeData(
+        color: AppColors.gray700,
+      ),
+
+      // etc
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: AppColors.primary,
+      colorScheme: ColorScheme(
+        brightness: Brightness.dark,
+        primary: AppColors.primary,
+        onPrimary: AppColors.white,
+
+        secondary: AppColors.accent,
+        onSecondary: AppColors.white,
+
+        surface: AppColors.gray800,
+        onSurface: AppColors.gray100,
+
+        error: AppColors.errorBase,
+        onError: AppColors.white,
+      ),
+      scaffoldBackgroundColor: AppColors.gray900,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.gray900,
+        foregroundColor: AppColors.white,
+        elevation: 0,
+      ),
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+          color: AppColors.gray100,
+          fontSize: 16,
+        ),
+      ),
+      iconTheme: const IconThemeData(
+        color: AppColors.gray400,
+      ),
+    );
+  }
+}
