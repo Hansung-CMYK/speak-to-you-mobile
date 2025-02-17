63fb6c784c3b188724f0c18f354be591fbdebc15
junni01kim <audwnssp@naver.com>
2025-02-18 01:49:46 +0900

    feat: Add ImageButton Widget

--- lib/widgets/appbar/main_app_bar.dart
+++ lib/widgets/appbar/main_app_bar.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../theme/color.dart';
 import '../../theme/theme.dart';
+import '../ImageButton.dart';
 
 /// [MainAppBar]는 SpeakToYou에서 가장 기본적으로 사용되는 AppBar이다.
 ///
@@ -27,47 +28,57 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(0, 0, 8.0, 0),
+      padding: EdgeInsets.fromLTRB(0, 0, 18.0, 0),
       child: AppBar(
-        title: DefaultTabController( // TabBar를 AppBar의 좌측에 배치하기 위함
+        toolbarHeight: kToolbarHeight + 70,
+        title: DefaultTabController(
+          // TabBar를 AppBar의 좌측에 배치하기 위함
           length: tabController.length, // 이용할 Tab은 2개로 지정
           child: TabBar(
             controller: tabController,
-            isScrollable: true, // TabBar의 너비를 지정할 수 있게 한다.(Default: Maximum Width)
-            tabAlignment: TabAlignment.start, // TabBar의 Tab들을 왼쪽으로 정렬시킨다. (Default: Center)
-            labelPadding: EdgeInsets.symmetric(horizontal: 8.0), // 각 텍스트 간의 간격 지정
+            isScrollable:
+                true, // TabBar의 너비를 지정할 수 있게 한다.(Default: Maximum Width)
+            tabAlignment: TabAlignment
+                .start, // TabBar의 Tab들을 왼쪽으로 정렬시킨다. (Default: Center)
+            labelPadding:
+                EdgeInsets.symmetric(horizontal: 8.0), // 각 텍스트 간의 간격 지정
             indicator: BoxDecoration(), // 선택된 페이지의 밑줄 하이라이팅 제거
-            labelStyle: TextStyle( // 선택된 Tab의 텍스트 속성
+            labelStyle: TextStyle(
+              // 선택된 Tab의 텍스트 속성
               color: AppColors.black,
               fontSize: 21,
               fontWeight: FontWeight.bold,
             ),
-            unselectedLabelStyle: TextStyle( // 선택되지 않은 Tab의 텍스트 속성
+            unselectedLabelStyle: TextStyle(
+              // 선택되지 않은 Tab의 텍스트 속성
               color: AppColors.gray400,
               fontSize: 21,
               fontWeight: FontWeight.bold,
             ),
-            tabs: [ // 지정된 Tab * 알림과 설정은 탭이 아닌 버튼을 통해 다른 페이지로 이동한다.
+            tabs: [
+              // 지정된 Tab * 알림과 설정은 탭이 아닌 버튼을 통해 다른 페이지로 이동한다.
               Tab(text: "스피크"),
               Tab(text: "캘린더"),
             ],
           ),
         ),
-        actions: [ // '알림'과 '설정'을 AppBar의 우측에 배치하기 위한
+        actions: [
+          // '알림'과 '설정'을 AppBar의 우측에 배치하기 위한
           Row(
             children: [
-              IconButton( // 알림 페이지로 이동하기 위한 Button
+              IconButton(
+                // 알림 페이지로 이동하기 위한 Button
                 icon: Icon(Icons.alarm), // TODO: 디자인 확정되면 변경할 것
                 iconSize: 30,
                 onPressed: alertMethod,
               ),
-              IconButton( // 설정 페이지로 이동하기 위한 Button
-                icon: ImageIcon(Image.asset( // TODO: 이미지 리소스 조회 방식에 맞춰 변경할 것
-                  egoIconPath,
-                  scale: 9.0,
-                ).image),
-                iconSize: 30,
-                onPressed: settingsMethod,
+              ImageButton(
+                // 설정 페이지로 이동하기 위한 Button
+                imagePath: egoIconPath,
+                onTab: settingsMethod,
+                width: 35.0,
+                height: 35.0,
+                radius: 100.0,
               ),
             ],
           ),
@@ -78,7 +89,7 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   /// AppBar의 사이즈 조정을 위한 필수 getter
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 70);
 }
 
 /// AppBar 단위 테스트 코드
@@ -139,12 +150,13 @@ class _SampleMainAppBarScreenState extends ConsumerState<SampleMainAppBarScreen>
     return Scaffold(
       /// 각 Screen으로 이동하기 위한 Navigator AppBar이다.
       appBar: MainAppBar(_tabController),
+
       /// MainAppBar에서 선택된 Tab의 Screen이 나타나는 영역이다.
       body: TabBarView(
         controller: _tabController,
         children: [
-          TmpSpeakScreen(),
-          TmpCalendarScreen(),
+          TmpSpeakScreen(), // TODO: 스피크 스크린에 연결
+          TmpCalendarScreen(), // TODO: 캘린더 스크린에 연결
         ],
       ),
     );
