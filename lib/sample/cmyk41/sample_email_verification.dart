14f00f9baf84c75e7c0a12ca89d5ca49449cd6c4
6-keem <6ukeem@gmail.com>
2025-02-19 04:52:48 +0900

    test: Add email verification sample screen

---
+++ lib/sample/cmyk41/sample_email_verification.dart
@@ -0,0 +1,29 @@
+import 'package:ego/sample/cmky10/sample_stack_app_bar.dart';
+import 'package:ego/screens/email_sent.dart';
+import 'package:ego/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    SampleEmailVerification(),
+  );
+}
+
+class SampleEmailVerification extends StatelessWidget {
+  const SampleEmailVerification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(393, 852),
+      builder: (context, child) => MaterialApp(
+          title: 'Ego',
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: EmailSentScreen(
+            nextPage: SampleStackAppBarScreen(),
+          )),
+    );
+  }
+}
