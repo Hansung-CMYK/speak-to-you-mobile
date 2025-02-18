de509bd3a7c95e2e526dcfeb1bdf32bc98f8b2cd
6-keem <6ukeem@gmail.com>
2025-02-19 04:34:00 +0900

    feat: Add email sent screen

--- lib/screens/email_verification.dart
+++ lib/screens/email_verification.dart
@@ -1,4 +1,5 @@
 import 'package:ego/theme/color.dart';
+import 'package:ego/widgets/appbar/stack_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
