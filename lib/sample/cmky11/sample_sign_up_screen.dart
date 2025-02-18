5e51b46ad944071fba865c2e043a379ce094c4f7
junni01kim <audwnssp@naver.com>
2025-02-19 02:25:09 +0900

    feat: Add to screenutil

--- lib/sample/cmky11/sample_sign_up_screen.dart
+++ lib/sample/cmky11/sample_sign_up_screen.dart
@@ -4,6 +4,7 @@ import 'package:ego/sample/cmky11/tmpscreen/tmp_terms_of_use_screen.dart';
 import 'package:ego/screens/signup/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../theme/theme.dart';
 
@@ -20,17 +21,20 @@ class SignUpScreenTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '앱 바 테스트 페이지',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      initialRoute: 'SignUp',
-      routes: {
-        'SignUp': (context) => SignUpScreen(),
-        'SendMail': (context) => TmpSendMailScreen(),
-        'TermsOfUse': (context) => TmpTermsOfUseScreen(),
-        'PrivacyPolicy': (context) => TmpPrivacyPolicyScreen(),
-      },
+    return ScreenUtilInit(
+      designSize: Size(393, 852),
+      builder: (context, child) => MaterialApp(
+        title: '앱 바 테스트 페이지',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        initialRoute: 'SignUp',
+        routes: {
+          'SignUp': (context) => SignUpScreen(),
+          'SendMail': (context) => TmpSendMailScreen(),
+          'TermsOfUse': (context) => TmpTermsOfUseScreen(),
+          'PrivacyPolicy': (context) => TmpPrivacyPolicyScreen(),
+        },
+      ),
     );
   }
 }
