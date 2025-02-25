de0a61609cbae67e3ae27108360aae3303af47c8
junni01kim <audwnssp@naver.com>
2025-02-26 01:21:35 +0900

    feat: Add table_calendar library

--- lib/screens/record/calendar/calendar_screen.dart
+++ lib/screens/record/calendar/calendar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatelessWidget {
   @override
@@ -6,7 +7,12 @@ class CalendarScreen extends StatelessWidget {
     return Container(
       child: Column(
         children: [
-
+          TableCalendar(
+            locale: 'ko_KR',
+            firstDay: DateTime(2000, 1, 1),
+            lastDay: DateTime(2025, 12, 31), // TODO: 수정할 것
+            focusedDay: DateTime.now(),
+          ),
         ],
       )
     );
