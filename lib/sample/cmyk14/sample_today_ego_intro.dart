aa709646bf842a8db7eb405ec29bf311c6b4ce25
KJH0506 <pete05006017@gmail.com>
2025-02-25 23:28:01 +0900

    feat: specific Container 구현
    - EGO의 성격, 소개를 확인 할 수 있는 Container 추가
    - 확인 버튼 추가

--- lib/sample/cmyk14/sample_today_ego_intro.dart
+++ lib/sample/cmyk14/sample_today_ego_intro.dart
@@ -6,9 +6,7 @@ import 'package:ego/theme/theme.dart';
 import 'package:ego/widgets/bottomsheet/today_ego_intro.dart';
 
 void main() {
-  runApp(
-    SampleTodayEgoIntro(),
-  );
+  runApp(SampleTodayEgoIntro());
 }
 
 class SampleTodayEgoIntro extends StatelessWidget {
@@ -17,13 +15,15 @@ class SampleTodayEgoIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: Size(393, 852),
-        builder: (context, child) => MaterialApp(
-          title: 'Ego',
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          home: MyHomePage(),
-        ));
+      designSize: Size(393, 852),
+      builder:
+          (context, child) => MaterialApp(
+            title: 'Ego',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            home: MyHomePage(),
+          ),
+    );
   }
 }
 
@@ -34,7 +34,18 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(title: Text('Check Today Ego!!')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
+          onPressed: () async {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return Center(child: CircularProgressIndicator());
+              },
+            );
+
+            // API 호출 시뮬레이션
+            await Future.delayed(Duration(seconds: 2));
+
             showTodayEgoIntroSheet(context);
           },
           child: Text('Check Today EGO'),
