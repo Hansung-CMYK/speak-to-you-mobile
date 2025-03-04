fa1b34cf62dcb0bb9a18d7e93ac3f44a48c8885a
junni01kim <audwnssp@naver.com>
2025-03-05 00:10:02 +0900

    feat: Add flutter_html

--- lib/screens/notice/notice_detail_screen.dart
+++ lib/screens/notice/notice_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:ego/theme/color.dart';
 import 'package:ego/widgets/appbar/stack_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 
@@ -20,21 +21,23 @@ class NoticeDetailScreen extends StatelessWidget {
   }
 
   Widget _body() {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: 12.h,
-        horizontal: 20.w,
-      ),
-      child: Column(
-        spacing: 24.h,
-        children: [
-          _title(),
-          Divider(
-            indent: 1,
-            color: AppColors.gray200,
-          ),
-          _content(),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 12.h,
+          horizontal: 20.w,
+        ),
+        child: Column(
+          spacing: 24.h,
+          children: [
+            _title(),
+            Divider(
+              indent: 1,
+              color: AppColors.gray200,
+            ),
+            _content(),
+          ],
+        ),
       ),
     );
   }
@@ -75,26 +78,18 @@ class NoticeDetailScreen extends StatelessWidget {
   }
 
   Widget _content() {
-    return Text(
-      SAMPLE_TEXT,
-      style: TextStyle(
-        color: AppColors.black,
-        fontSize: 14.sp,
-        fontWeight: FontWeight.w500,
-      ),
+    return Html(
+      data: SAMPLE_HTML,
     );
   }
 }
 
-String SAMPLE_TEXT = """
-설 연휴 기간 동안 
-[휴무 일정: 예시 - 2월 9일(금) ~ 2월 12일(월)] 운영이 중단되며, 
-해당 기간 동안 접수된 문의는 
-연휴 이후 순차적으로 처리될 예정입니다.
-
-가족, 친지와 함께 행복한 명절 보내시고, 
-새해에는 건강과 행운이 가득하시길 바랍니다. 
-올 한 해도 많은 관심과 성원 부탁드립니다.
-
-새해 복 많이 받으세요! 🎉🙏
+String SAMPLE_HTML = """
+<h2>HTML이 적용된 영역입니다.</h2>
+<p>설 연휴 기간 동안 [휴무 일정: 예시 - 2월 9일(금) ~ 2월 12일(월)] 운영이 중단되며,<br>
+   해당 기간 동안 접수된 문의는 연휴 이후 순차적으로 처리될 예정입니다.</p>
+<p>가족, 친지와 함께 행복한 명절 보내시고,<br>
+   새해에는 건강과 행운이 가득하시길 바랍니다.<br>
+   올 한 해도 많은 관심과 성원 부탁드립니다.</p>
+<p><strong>새해 복 많이 받으세요! 🎉🙏</strong></p>
 """;
