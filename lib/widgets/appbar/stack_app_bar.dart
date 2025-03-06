4f7d4b35e8423b7b215f389b358482a701cb02a1
junni01kim <audwnssp@naver.com>
2025-03-06 09:34:15 +0900

    fix: resolve color issue on scroll

--- lib/widgets/appbar/stack_app_bar.dart
+++ lib/widgets/appbar/stack_app_bar.dart
@@ -1,5 +1,9 @@
+import 'package:ego/widgets/button/svg_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../theme/color.dart';
 
 /// [StackAppBar]는 StackNavigation을 이용한 스크린에서 사용되는 AppBar이다.
 ///
@@ -7,6 +11,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class StackAppBar extends StatelessWidget implements PreferredSizeWidget {
   /// 스크린의 이름이다.
   final String title;
+
+  final String _backarrowPath = 'assets/icon/arrow_back.svg';
 
   /// [title] AppBar 중앙에 나타날 문구 주입 (스크린 명 권장). default: ""
   const StackAppBar({super.key, this.title = ""});
@@ -22,23 +28,24 @@ class StackAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(
-        vertical: 12.w,
+        vertical: 16.w,
       ),
       child: AppBar(
         centerTitle: true, // AppBar의 제목 중앙 정렬
-        leading: IconButton(
-          // 뒤로가기 버튼
-          onPressed: () => backScreenMethod(context),
-          highlightColor: Colors.transparent, // 터치 애니메이션 제거
-          icon: Icon(
-            Icons.arrow_back, // 뒤로가기 아이콘 (안드로이드 버전)
-            size: 24.w,
+        surfaceTintColor: AppColors.white,
+        leading: GestureDetector(
+          onTap: () => backScreenMethod(context),
+          child: Center(
+            widthFactor: 24.w,
+            heightFactor: 24.h,
+            child: SvgPicture.asset(_backarrowPath),
           ),
         ),
         title: Text(
           // 페이지의 제목
           title,
           style: TextStyle(
+            color: AppColors.gray900,
             fontSize: 16.sp,
             fontWeight: FontWeight.w600,
           ),
