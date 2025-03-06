30575ef50a97e2a33f65fe97a84c73215f5fb17d
6-keem <6ukeem@gmail.com>
2025-03-06 09:03:43 +0900

    feat: Add setting sample screen

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -1,34 +1,8 @@
 import 'package:ego/theme/color.dart';
-import 'package:ego/theme/theme.dart';
 import 'package:ego/types/dialog_type.dart';
-import 'package:ego/widgets/appbar/stack_app_bar.dart';
 import 'package:ego/widgets/confirm_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-void main() {
-  runApp(const FigmaToCodeApp());
-}
-
-class FigmaToCodeApp extends StatelessWidget {
-  const FigmaToCodeApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Size(393, 852),
-      builder:
-          (context, child) => MaterialApp(
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            home: Scaffold(
-              appBar: StackAppBar(title: '설정'),
-              body: SingleChildScrollView(child: Setting()),
-            ),
-          ),
-    );
-  }
-}
 
 class Setting extends StatelessWidget {
   @override
