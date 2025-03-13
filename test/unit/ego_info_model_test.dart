8db51ee9c9e576335e965ad6c110a46dc1a4618c
KJH0506 <pete05006017@gmail.com>
2025-03-13 18:44:34 +0900

    [test-code-ex] test : EgoInfoModel의 calcRemainingDays() 코드 수정

--- test/unit/ego_info_model_test.dart
+++ test/unit/ego_info_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:ego/models/ego_info_model.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
 
 /// 임시 EGO Model 테스트 입니다.
 ///
@@ -10,17 +11,20 @@ void main() {
       id: '1',
       egoIcon: '',
       egoName: 'Power',
-      egoBirth: '2025/03/12',
+      egoBirth: DateFormat('yyyy/MM/dd').format(DateTime.now().add(Duration(days: 0))),
       egoPersonality: '착함, 활발',
       egoSelfIntro: '안녕, 나는 Test EGO야',
     );
 
-    test('toJson() 테스트', () {
+    /// 'EgoInfoModel_Serialization_MatchesExpectedValues' 테스트 시나리오
+    ///
+    /// 위에서 정의한 egoInfoModel이 직렬화가 잘 이루어지는지 확인
+    test('EgoInfoModel_Serialization_MatchesExpectedValues', () {
       final expectedJson = {
         'id': '1',
         'egoIcon': '',
         'egoName': 'Power',
-        'egoBirth': '2025/03/12',
+        'egoBirth': DateFormat('yyyy/MM/dd').format(DateTime.now()),
         'egoPersonality': '착함, 활발',
         'egoSelfIntro': '안녕, 나는 Test EGO야',
       };
@@ -28,19 +32,25 @@ void main() {
       expect(egoInfoModel.toJson(), equals(expectedJson));
     });
 
-    test('fromJson() 테스트', () {
+    /// 'EgoInfoModel_Deserialization_MatchesExpectedValues' 테스트 시나리오
+    ///
+    /// 위에서 정의한 egoInfoModel이 역직렬화가 잘 이루어지는지 확인
+    test('EgoInfoModel_Deserialization_MatchesExpectedValues', () {
       final ego = EgoInfoModel.fromJson(egoInfoModel.toJson());
 
       expect(ego.id, '1');
       expect(ego.egoIcon, '');
       expect(ego.egoName, 'Power');
-      expect(ego.egoBirth, '2025/03/12');
+      expect(ego.egoBirth, DateFormat('yyyy/MM/dd').format(DateTime.now()));
       expect(ego.egoPersonality, '착함, 활발');
       expect(ego.egoSelfIntro, '안녕, 나는 Test EGO야');
     });
 
-    test('EGO 생성일로부터 현재 날짜', () {
-      expect(egoInfoModel.calcRemainingDays(), 1);
+    /// 'calcRemainingDays_fromToday_zero' 테스트 시나리오
+    ///
+    /// 오늘이 EGO의 생일인 경우 D-0이 나와냐 하므로 expect 값을 0으로 설정
+    test('calcRemainingDays_fromToday_zero', () {
+      expect(egoInfoModel.calcRemainingDays(), 0);
     });
   });
 }
