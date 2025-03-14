9c9d220293b647dca59a26b25e766a83daa85ab1
KJH0506 <pete05006017@gmail.com>
2025-03-14 14:37:25 +0900

    [cmyk-38] feat : 이미지 공유 기능 추가
    - 전체 일기 공유 버튼 : 텍스트 공유
    - 하나의 일기 공유 버튼 : 이미지 공유

--- lib/screens/diary/share_one_diary.dart
+++ lib/screens/diary/share_one_diary.dart
@@ -1,12 +1,26 @@
+import 'package:flutter/services.dart';
+
 import 'package:ego/widgets/customtoast/custom_toast.dart';
+
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
-/// 특정 일기를 공유하는 함수 입니다.
-///
 Future<void> shareOneDiary(String text, CustomToast customToast) async {
-  final result = await Share.share(text);
+  try {
+    final ByteData bytes = await rootBundle.load('assets/image/first_diary_sample_image.png');
+    final Uint8List uint8List = bytes.buffer.asUint8List();
 
-  if (result.status == ShareResultStatus.success) {
-    customToast.showTopToast();
+    final tempDir = await getTemporaryDirectory();
+    final tempFile = File('${tempDir.path}/first_diary_sample_image.png');
+    await tempFile.writeAsBytes(uint8List);
+
+    final result = await Share.shareXFiles([XFile(tempFile.path)], text: text);
+
+    if (result.status == ShareResultStatus.success) {
+      customToast.showTopToast();
+    }
+  } catch (e) {
+    print('에러 발생: $e');
   }
 }
