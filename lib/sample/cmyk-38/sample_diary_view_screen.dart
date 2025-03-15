72c1124e5a684345ebdf63de6f26743fa3ff77ab
KJH0506 <pete05006017@gmail.com>
2025-03-15 17:16:15 +0900

    [test-code-ex] feat : 일기 확인 화면에서 이미지 재생성 api
    - 이미지 재생성시 임시 이미지를 불러 와서 업데이트하는 로직 추가

---
+++ lib/sample/cmyk-38/sample_diary_view_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../screens/diary/diary_view_screen.dart';
+import '../../theme/theme.dart';
+
+void main() {
+  runApp(const DiaryViewApp());
+}
+
+class DiaryViewApp extends StatelessWidget {
+  const DiaryViewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      builder: (context, child) => MaterialApp(
+        title: 'Ego',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: SampleHomeScreen()
+      ),
+    );
+  }
+}
+
+class SampleHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('홈 화면')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DiaryViewScreen()),
+            );
+          },
+          child: const Text('Diary 화면으로 이동'),
+        ),
+      ),
+    );
+  }
+}
+
