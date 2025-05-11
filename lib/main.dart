55e21af40ccff96f8fac502eafbfee2a0c6f7ea5
KJH0506 <pete05006017@gmail.com>
2025-05-12 00:33:42 +0900

    [cmyk-196] : FCM 수신 로직 작성
    - build.gradle 수정
    - FCM service로직 작성
    - 띄워지는 Notification 로직 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:ego/firebase_options.dart';
+import 'package:ego/services/firebase_messaging_service.dart';
+import 'package:ego/services/local_notifications_service.dart';
 import 'package:ego/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,7 +10,17 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 import 'screens/ex_home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final localNotificationsService = LocalNotificationsService.instance();
+  await localNotificationsService.init();
+
+  final firebaseMessagingService = FirebaseMessagingService.instance();
+  await firebaseMessagingService.init(localNotificationsService: localNotificationsService);
+
   runApp(
     ProviderScope(
       child: MyApp(),
