efdf021a11af780f4e1f31b784f75d26ac18d151
KJH0506 <pete05006017@gmail.com>
2025-05-13 20:20:24 +0900

    [cmyk-199] : 내림,오름 차순 정렬 로직 추가

--- lib/screens/egolistview/ego_list_blurred_screen.dart
+++ lib/screens/egolistview/ego_list_blurred_screen.dart
@@ -38,6 +38,7 @@ class _BlurredListScreenState extends ConsumerState<BlurredListScreen> {
   int _pageNum = 0;
   bool _isLoading = false;
   bool _hasMore = true;
+  bool _isAscending = true; // 정렬 방향 true: 오름차순, false: 내림차순
 
   @override
   void initState() {
@@ -126,7 +127,14 @@ class _BlurredListScreenState extends ConsumerState<BlurredListScreen> {
   // 정렬 방식 선택
   void _selectSort(String sort) {
     setState(() {
-      selectedSort = sort;
+      if (selectedSort == sort) {
+        // 같은 정렬을 다시 누르면 방향 변경
+        _isAscending = !_isAscending;
+      } else {
+        // 정렬 방식이 바뀌면 오름차순으로 초기화
+        selectedSort = sort;
+        _isAscending = true;
+      }
       _applySort();
     });
   }
@@ -135,17 +143,24 @@ class _BlurredListScreenState extends ConsumerState<BlurredListScreen> {
   void _applySort() {
     switch (selectedSort) {
       case '이름순':
-        filteredList.sort((a, b) => a.name.compareTo(b.name));
+        filteredList.sort((a, b) => _isAscending
+            ? a.name.compareTo(b.name)
+            : b.name.compareTo(a.name));
         break;
-      case '최신대화순': // 최초 API 응답값 자체가 시간순이라 _egoList로 filteredList의 값을 변경하면 됨
+      case '최신대화순':
         filteredList = List.from(_egoList);
+        if (!_isAscending) {
+          filteredList = filteredList.reversed.toList();
+        }
         break;
     }
   }
 
+
   // 정렬 옵션 버튼 builder
   Widget _buildSortButton(String option) {
     final isSelected = selectedSort == option;
+
     return GestureDetector(
       onTap: () => _selectSort(option),
       child: Container(
@@ -165,17 +180,31 @@ class _BlurredListScreenState extends ConsumerState<BlurredListScreen> {
             ),
           ],
         ),
-        child: Text(
-          option,
-          style: TextStyle(
-            color: AppColors.white,
-            fontWeight: FontWeight.w600,
-            fontSize: 14.sp,
-          ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              option,
+              style: TextStyle(
+                color: AppColors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 14.sp,
+              ),
+            ),
+            if (isSelected) ...[
+              SizedBox(width: 6.w),
+              Icon(
+                _isAscending ? Icons.arrow_upward : Icons.arrow_downward,
+                color: AppColors.white,
+                size: 16.sp,
+              ),
+            ],
+          ],
         ),
       ),
     );
   }
+
 
   @override
   Widget build(BuildContext context) {
