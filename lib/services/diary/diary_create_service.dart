7c370be595aa90188be661b9e3a9d942480fbb99
KJH0506 <pete05006017@gmail.com>
2025-05-19 22:48:57 +0900

    [cmyk-231] : 일기 생성 API연결

--- lib/services/diary/diary_create_service.dart
+++ lib/services/diary/diary_create_service.dart
@@ -8,7 +8,23 @@ import 'package:ego/utils/constants.dart';
 
 class DiaryCreateService{
 
+  // 일기 생성 요청
+  static Future<Diary> sendDiaryRequest(DiaryRequestModel request) async {
+    final response = await http.post(
+      Uri.parse(diaryCreateUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(request.toJson()),
+    );
 
+    if (response.statusCode == 200) {
+      final json = utf8.decode(response.bodyBytes); // 한글 깨짐 방지
+      final decodedJson = jsonDecode(json);
+      final diary = Diary.fromJson(decodedJson['data']);
 
+      return diary;
+    } else {
+      throw Exception('일기 생성 실패: ${response.body}');
+    }
+  }
 
 }
